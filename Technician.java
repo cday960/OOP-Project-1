23d9c3caf0d62a029afccb7f718b7e4d37ec1827
Fenlawed <129792206+Fenlawed@users.noreply.github.com>
2023-04-26 10:38:18 -0500

    Update newEmployee to make specific Librarian or Technician

--- Technician.java
+++ Technician.java
@@ -1,3 +1,4 @@
+import java.io.LineNumberReader;
 import java.util.ArrayList;
 
 public class Technician extends Employee
@@ -37,29 +38,52 @@ public class Technician extends Employee
     System.out.println("Error: item removal failed, not present in collection");
   }
 
-  public void newEmployee(
-    String name, 
-    String address, 
-    String dob, 
-    String email, 
-    String ssn, 
-    ArrayList<Employee> employeeList
-  ){
-    Employee newEmployee = new Employee(name, address, dob, email, ssn);
-    employeeList.add(newEmployee);
+  public void newEmployee(String name, String address, String dob, String email, String ssn, ArrayList<Employee> employeeList)
+  {
+    System.out.println("What type of Employee : Librarian or Technician");//Decides if employee is Librarian or Emplyee
+    String employeeType = scanner.nextLine();
+    if(!(employeeType.equals("Librarian")) && !(employeeType.equals("Technician")))//If incorrect input
+    {
+            while(!(employeeType.equals("Librarian")) && !(employeeType.equals("Technician")))//Go again until right
+            {
+                System.out.println("Invalid Employee Type. Insert either Librarian or Technician: ");
+                employeeType = scanner.nextLine();
+            }
+    }
+    if(employeeType.equals("Librarian"))//If Librarian
+    {
+      Librarian newEmployee = new Librarian(name, address, dob, email, ssn);//Make new Librarian
+      employeeList.add(newEmployee);
+    }
+    if(employeeType.equals("Technician"))//If technician
+    {
+      Technician newEmployee = new Technician(name,address,dob,email,ssn);//Make new technician
+      employeeList.add(newEmployee);
+    }
   }
 
-  public void newEmployee(
-    String name, 
-    String address, 
-    String dob, 
-    String email, 
-    String ssn, 
-    String empID,
-    float salary,
-    ArrayList<Employee> employeeList
-  ){
-    Employee newEmployee = new Employee(name, address, dob, email, ssn, empID, salary);
-    employeeList.add(newEmployee);
+  public void newEmployee(String name, String address, String dob, String email, String ssn, String empID, float salary, ArrayList<Employee> employeeList)
+  {
+    System.out.println("What type of Employee : Librarian or Techician");//Decides if employee is a Librarian or Technician
+    String employeeType = scanner.nextLine();
+    if(!(employeeType.equals("Librarian")) && !(employeeType.equals("Technician")))//Checks if it is either type
+    {
+            while(!(employeeType.equals("Librarian")) && !(employeeType.equals("Technician")))//If not go again
+            {
+                System.out.println("Invalid Employee Type. Insert either Librarian or Technician: ");
+                employeeType = scanner.nextLine();
+            }
+    }
+    if(employeeType.equals("Librarian"))//If Librarian
+    {
+      Librarian newEmployee = new Librarian(name, address, dob, email, ssn, empID, salary);//Make new Librarian
+      employeeList.add(newEmployee);
+    }
+    if(employeeType.equals("Technician"))//If Technician
+    {
+      Technician newEmployee = new Technician(name,address,dob,email,ssn,empID,salary);//Make Technician
+      employeeList.add(newEmployee);
+    }
   }
+
 }
